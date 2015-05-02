8ce15c43dcb53be052f0b60fccf45db0fe6ea4c1
Joseph Heenan <josephheenan@Josephs-MacBook-Pro.local>
2015-05-02 13:42:32 -0400

    Initial Commit

---
+++ SensorTagTechcrunchTests/SensorTagTechcrunchTests.swift
@@ -0,0 +1,36 @@
+//
+//  SensorTagTechcrunchTests.swift
+//  SensorTagTechcrunchTests
+//
+//  Created by Joseph Heenan on 5/2/15.
+//  Copyright (c) 2015 Joseph Heenan. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SensorTagTechcrunchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
